f8d6e339ca5fa2def357b7e517efaa02bcbaa0ba
Highalnd <highland@402945786.com>
2021-08-07 12:25:32 +0800

    add batch script

---
+++ resnet18-mnist/runAll.py
@@ -0,0 +1,5 @@
+import os
+os.system("python ./resnet18-mnist-reverse-train-forgetOneKind-3-10.py")
+os.system("python ./resnet18-mnist-reverse-train-forgetOneKind-3-11.py")
+os.system("python ./resnet18-mnist-reverse-train-forgetOneKind-3-12.py")
+os.system("python ./resnet18-mnist-reverse-train-forgetOneKind-3-13.py")
